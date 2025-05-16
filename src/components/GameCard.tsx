daf163c94793d24a99f2f5ac9a0ccaab74665d10
Nemanja1996S <savic1996.neca@gmail.com>
2025-05-16 20:31:58 +0200

    Displaying paltform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,16 +1,21 @@
 import type { Game } from "@/hooks/useGames"
 import { Card, Image, Heading } from "@chakra-ui/react"
+import PlatformIconList from "./PlatformIconList"
+
+
 
 interface Props {
     game: Game
 }
 
 const GameCard = ({game} : Props) => {
+
   return (
     <Card.Root borderRadius="10px" margin={15} overflow="hidden">
         <Image src={game.background_image} />
         <Card.Body>
            <Heading fontSize="2xl">{game.name}</Heading>
+           <PlatformIconList platforms={game.parent_platforms.map( p => p.platform)} />
         </Card.Body>
         <Card.Footer />
     </Card.Root>
